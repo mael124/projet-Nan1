f31847e5382feedc94b4beb6ef65e999eb97c7ea
mael124 <59884065+mael124@users.noreply.github.com>
2020-03-16 18:32:31 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function() {
+    $('.btn-nav-content').on('click', function() {
+      $('.sideBar').toggleClass('activeSidebar');
+      $('.aaa').toggleClass('bodysidebar');
+      $('.btn-nav-content').toggleClass('btn-nav');
+    });
+  })();
